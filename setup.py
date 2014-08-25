feb4c437005f7fd0342c54ed8c8d3decd94441e9
neoinsanity <mindbender@gmail.com>
2014-08-24 23:10:57 -0700

    Updated dev version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.txt') as file:
 
 setup(
     name='existenz',
-    version='0.0.1a',
+    version='0.0.1b',
     author='Raul Gonzalez',
     author_email='mindbender@gmail.com',
     url='https://github.com/neoinsanity/existenz',
