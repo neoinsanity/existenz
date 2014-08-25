80c9d5e440cee1761286b48410aa34459b328e1d
neoinsanity <mindbender@gmail.com>
2014-08-24 23:05:04 -0700

    Initial project setup.

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""The setuptools setup file."""
+from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name='existenz',
+    version='0.0.1a',
+    author='Raul Gonzalez',
+    author_email='mindbender@gmail.com',
+    url='https://github.com/neoinsanity/existenz',
+    license='Apache License 2.0',
+    description='an existing individual',
+    long_description=long_description,
+    packages=['existenz',],
+    install_requires=[],
+    include_package_data = True,
+)
